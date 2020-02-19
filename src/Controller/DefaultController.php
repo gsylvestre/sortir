6bc7cb50d718eea64497a5c3239f945bd55a303a
guillaume <gsylvestre@gmail.com>
2020-02-19 09:01:18 +0100

    remove mapboxhelper, add comments

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,20 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Event;
-use App\Geolocation\MapBoxHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Gestion des pages types "divers"
+ *
+ * Class DefaultController
+ * @package App\Controller
+ */
 class DefaultController extends AbstractController
 {
     /**
+     * Page d'accueil du site
+     *
      * @Route("/", name="home")
      */
-    public function home(MapBoxHelper $mapbox)
+    public function home()
     {
+        //juste pour récupérer le nombre total de sorties
         $eventRepo = $this->getDoctrine()->getRepository(Event::class);
+        //sans filtre avec []
         $allEventCount = $eventRepo->count([]);
-
 
         return $this->render('default/home.html.twig', [
             "allEventCount" => $allEventCount,
