d4fd26ee3a360f2acd4ae8ae0a1503762f60c65b
guillaume <gsylvestre@gmail.com>
2020-02-20 15:25:13 +0100

    add comments

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -41,6 +41,7 @@ class UserController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $user = $this->getUser();
 
+        //on garde une référence vers l'éventuelle photo de profil précédente
         $previousProfilePicture = $user->getPicture();
 
         $form = $this->createForm(ProfileUploadType::class, $user);
@@ -48,33 +49,45 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted()) {
             if ($form->isValid()) {
+                //génère un nom de fichier aléatoire et unique
                 $safeFilename = bin2hex(random_bytes(10)) . uniqid();
+                //on rajoute l'extension du fichier
                 $newFilename = $safeFilename.'.'.$user->getPictureUpload()->guessExtension();
 
+                //on déplace le fichier vers notre destination
+                //profile_pic_dir est défini dans config/services.yaml pour éviter d'avoir un chemin qui ressemble à ../../../../uploads !
                 $user->getPictureUpload()->move($this->getParameter('profile_pic_dir'), $newFilename);
 
+                //on hydrate le nom du fichier dans le user
                 $user->setPicture($newFilename);
 
+                //on enlève l'UploadedFile sinon ça bugue avec la sérialization
                 $user->setPictureUpload(null);
+
+                //on persiste le user
                 $em->persist($user);
                 $em->flush();
 
+                //s'il y avait une autre photo précédemment...
                 if (!empty($previousProfilePicture)){
                     //supprime la photo précédente, physiquement
                     unlink($this->getParameter('profile_pic_dir') . "/" . $previousProfilePicture);
                     $this->addFlash('success', 'Photo de profil modifiée !');
                 }
+                //sinon, c'est que c'est une nouvelle photo
                 else {
                     $this->addFlash('success', 'Photo de profil ajoutée !');
                 }
 
+                //on redirige vers le profil de ce user
                 return $this->redirectToRoute('user_profile', ["id" => $user->getId()]);
             }
 
+            //on enlève l'UploadedFile sinon ça bugue avec la sérialization
             $user->setPictureUpload(null);
         }
 
-
+        //on recharge le donnée du user depuis la bdd au cas où
         $em->refresh($user);
 
         return $this->render('user/upload.html.twig', [
