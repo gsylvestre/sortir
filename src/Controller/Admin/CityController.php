b9b90f82f9fd4789567f448b802e04b845f66dc2
Guillaume <gsylvestre@gmail.com>
2021-04-13 08:07:13 +0200

    admin city CRUD (with command)

---
+++ src/Controller/Admin/CityController.php
@@ -0,0 +1,99 @@
+<?php
+
+// ce fichier et tous les twigs et les form ont été généré avec la commande make:crud !
+// je n'ai fait que quelques adaptations mineures
+
+namespace App\Controller\Admin;
+
+use App\Entity\City;
+use App\Form\CityType;
+use App\Repository\CityRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/villes")
+ */
+class CityController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_city_index", methods={"GET"})
+     */
+    public function index(CityRepository $cityRepository): Response
+    {
+        //TODO: pagination
+
+        return $this->render('admin/city/index.html.twig', [
+            'cities' => $cityRepository->findBy([], ['name' => 'ASC'], 1000),
+        ]);
+    }
+
+    /**
+     * @Route("/ajouter", name="admin_city_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $city = new City();
+        $form = $this->createForm(CityType::class, $city);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($city);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_city_index');
+        }
+
+        return $this->render('admin/city/new.html.twig', [
+            'city' => $city,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_city_show", methods={"GET"})
+     */
+    public function show(City $city): Response
+    {
+        return $this->render('admin/city/show.html.twig', [
+            'city' => $city,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/modifier", name="admin_city_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, City $city): Response
+    {
+        $form = $this->createForm(CityType::class, $city);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_city_index');
+        }
+
+        return $this->render('admin/city/edit.html.twig', [
+            'city' => $city,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_city_delete", methods={"POST"})
+     */
+    public function delete(Request $request, City $city): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$city->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($city);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_city_index');
+    }
+}
---
+++ src/Form/CityType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('department')
+            ->add('insee')
+            ->add('zip')
+            ->add('lat')
+            ->add('lng')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => City::class,
+        ]);
+    }
+}
