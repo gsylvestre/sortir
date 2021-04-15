e6747422aac12741f65af488afb895cdb621d062
Guillaume <gsylvestre@gmail.com>
2021-04-15 13:45:14 +0200

    better event detail page

--- src/Entity/EventSubscription.php
+++ src/Entity/EventSubscription.php
@@ -21,7 +21,9 @@ class EventSubscription
      */
     public function prePersist()
     {
-        $this->setCreatedDate(new \DateTime());
+        if (empty($this->getCreatedDate())){
+            $this->setCreatedDate(new \DateTime());
+        }
     }
 
     /**
