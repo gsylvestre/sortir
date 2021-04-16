38ea2568ad71c32ade8e777009c6e8042e9d058a
Guillaume <gsylvestre@gmail.com>
2021-04-16 08:43:28 +0200

    add comments

--- src/Geolocation/MapBoxHelper.php
+++ src/Geolocation/MapBoxHelper.php
@@ -6,18 +6,29 @@ use GuzzleHttp\Client;
 
 class MapBoxHelper
 {
+    //devrait être secret !
     const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiZ3N5bHZlc3RyZSIsImEiOiJjazN3MHYzemUwcjRpM2xwaXVidGNwOTluIn0.oNngcvTobTdNcBgg3tcPtg";
 
+    /**
+     * En fonction d'une adresse, permet de récupérer les coordonnées (lat et lng)
+     */
     public function getAddressCoordinates(string $streetAddress, string $zip, string $city, ?string $country = "fr"): array
     {
+        //client http de la librairie externe Guzzle (maintenant, Symfony propose un Client HTTP par défaut)
         $client = new Client();
 
+        //préparation de l'URL à laquelle on fait une requête
         $search = urlencode("$streetAddress $zip $city");
         $token = self::MAPBOX_ACCESS_TOKEN;
         $url = "https://api.mapbox.com/geocoding/v5/mapbox.places/$search.json?access_token=$token&country=$country";
 
+        //on fait une reequête à mapbox.com
         $response = $client->get($url);
+
+        //on récupère le contenu de la réponse
         $json = $response->getBody();
+
+        //on transforme ce json en tableau
         $data = json_decode($json, true);
 
         //on retourne toujours le premier résultat
